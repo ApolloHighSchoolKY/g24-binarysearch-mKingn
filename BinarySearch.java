b22ed5dc35448c2204f80a6aca9d65cacf0bb473
mKingn <michael.king@stu.daviess.kyschools.us>
2025-04-16 14:10:17 +0000

    you

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,19 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+		int bottom=0;
+		int top=ray.length-1;
+		int middle=0;
 
+
+		while(ray[middle] < item){
+		
+		middle=(top+bottom)/2;
+			
+		bottom=middle+1;
+			
+		}
+		return ray[middle];
 	}
 }
